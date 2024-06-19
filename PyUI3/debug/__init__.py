d0d86a5c9710cd8499a4a232c7bf212ca09b5bf5
jhi2 <johndrostan@outlook.com>
2024-06-19 12:57:06 -0700

    A ton of stuff happened.

--- PyUI3/debug/__init__.py
+++ PyUI3/debug/__init__.py
@@ -78,13 +78,16 @@ from win32more.xaml import XamlApplication
 class Application(XamlApplication):
     def __init__(self,**kwrgs):
         super().__init__(**kwrgs)
+        print("App initalized.")
 class Window(Window):
     def __init__(self,**kwrgs):
         super().__init__(**kwrgs)
         self.roster = []
+        print("Window initalized.")
     def add(self,widget):
         self.roster.append(widget)
+        print(f"Widget added to roster: {widget}")
     def applyWidgets(self):
         for widget in self.roster:
             self.Content = widget
-    
+            print(f"Widget applied to window: {widget}")
