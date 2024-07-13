b86af19a320f9ac2c62210dfbd2387481c60a15c
jhi2 <johndrostan@outlook.com>
2024-07-13 11:28:39 -0700

    Fixed

--- PyUI3/__init__.py
+++ PyUI3/__init__.py
@@ -1,7 +1,9 @@
 """
 WinUI3 with Python!
 """
-__version__ = "4.0.4"
+
+__version__ = "5.0.0"
+#The never-ending list of imports HAHA
 from win32more.Microsoft.UI.Xaml.Controls import (
     AnimatedIcon,
     AutoSuggestBox,
@@ -15,8 +17,6 @@ from win32more.Microsoft.UI.Xaml.Controls import (
     CommandBar,
     CommandBarFlyout,
     ContentDialog,
-    
-    
     DatePicker,
     DropDownButton,
     Expander,
@@ -25,10 +25,7 @@ from win32more.Microsoft.UI.Xaml.Controls import (
     GridView,
     HyperlinkButton,
     Image,
-    
     InfoBar,
-    
-    
     ListView,
     MapControl,
     MediaPlayerElement,
@@ -44,17 +41,10 @@ from win32more.Microsoft.UI.Xaml.Controls import (
     ProgressRing,
     RadioButton,
     RatingControl,
-    
     RichEditBox,
     RichTextBlock,
     ScrollViewer,
     SemanticZoom,
-   
-    
-    
-    
-    
-    
     Slider,
     SplitButton,
     SplitView,
@@ -65,30 +55,46 @@ from win32more.Microsoft.UI.Xaml.Controls import (
     TextBox,
     TimePicker,
     ToggleSwitch,
-    
     ToggleSplitButton,
     ToolTip,
     TreeView,
     TwoPaneView,
-    
 )
+
 from win32more.Microsoft.UI.Xaml import Window
 from win32more.xaml import XamlApplication
 from win32more.Microsoft.UI.Xaml.Media import MicaBackdrop
-class ApplicationWindow():
+#The class for the window
+class ApplicationWindow:
     def __init__(self):
+        #Initalizes the roster,application,and adds a shortcut to the window
         self.roster = []
-        class app(XamlApplication):
-            def __init__(self):
-                super().__init__(appargs)
-            def OnLaunched(self):
-                self.win = Window(winargs)
-        self.application = app()
-    def add(self, item):
+        self.app = App()
+        self.win = self.app.win
+
+    def add(self, item): #Adds a new item to the window
         self.roster.append(item)
-    def run(self):
-        self.application.Start()
+
+    def run(self): #Starts the application
+        self.app.Start()
         for item in self.roster:
-            self.win.content = item
+            self.win.Content = item
         self.win.Activate()
+
+    def set_title(self, title): #Sets the window title
+        self.win.Title = str(title)
+
+    def set_mica(self, mode): #Turns on or off mica on the app
+        if mode:
+            self.win.SystemBackdrop = MicaBackdrop()
+        else:
+            self.win.SystemBackdrop = None
+#The code for a PyUI3 Binding Application, required by ApplicationWindow
+class App(XamlApplication):
+    def __init__(self):
+        super().__init__()
+
+    def OnLaunched(self, args):
+        self.win = Window()
+
 
