78a9a1cffa9292c71b4df01c40295f7fdd41bfe2
jhi2 <johndrostan@outlook.com>
2024-10-11 12:46:49 -0700

    comit

---
+++ PyUI3/styles.py
@@ -0,0 +1,2 @@
+def get_style(app, style_name):
+    return app.Current.Resources.Lookup(style_name)
--- PyUI3/test.py
+++ PyUI3/test.py
@@ -1,20 +1,9 @@
-import PyUI3
+import __init__ as PyUI3
 
-# Create a window
+
 window = PyUI3.ApplicationWindow()
-
-# Add some widgets
-label = PyUI3.Label(Content="Print Hello World!")
-button = PyUI3.Button(Content="Do it now!")
+ring = PyUI3.ProgressRing()
+window.add(ring)
 
 
-window.add()
-# Define a callback function for the button
-def button_click():
-    print("Hello World!")
-
-# Bind the callback to the button
-button.on_click = button_click
-
-# Start the main loop
 window.run()
